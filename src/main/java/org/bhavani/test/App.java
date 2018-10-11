0ccde53ab8bd9df21fa2d9a0e95e49bdcb359852
bhavani.indukuri <bhavani.indukuri@kony.com>
2018-10-11 21:18:39 +0530

    Commiting maven test Java project

---
+++ src/main/java/org/bhavani/test/App.java
@@ -0,0 +1,14 @@
+package org.bhavani.test;
+
+import org.slf4j.*; 
+/**
+ * 
+ * @author srccodes.com
+ * @version 1.0
+ */
+public class App {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("Hello World");
+    }
+}
